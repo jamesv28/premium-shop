cd6b7f82f63d6a3200b4aa5f796d3449e4f62ad3
James Volmert <james.volmert@gmail.com>
2025-03-05 18:13:36 -0700

    seeded db

--- components/shared/Product/product-card.tsx
+++ components/shared/Product/product-card.tsx
@@ -6,7 +6,7 @@ import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-sm border-none">
       <CardHeader className="p-0 items-center">
         <Link href={`/product/${product.slug}`}>
           <Image
